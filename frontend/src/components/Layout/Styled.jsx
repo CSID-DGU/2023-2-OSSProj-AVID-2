47158ce178b4d0afffc9f97051a9b72ebb0cf9fc
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-10-31 15:12:28 +0900

    :sparkles: Feat: Home화면 구성
    home화면 구성 및 로그인, 회원가입 버튼 구현. 로그인, 회원가입 페이지 추가 수정 필요.

---
+++ frontend/src/components/Layout/Styled.jsx
@@ -0,0 +1,58 @@
+import styled, { keyframes } from 'styled-components';
+
+// nav
+export const Container = styled.nav`
+  position: relative;
+  top: 0;
+  width: 100%;
+  height: 30px;
+  //z-index: 1;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  transition-timing-function: ease-in;
+  transition: all 0.5s;
+  background-color: white;
+`
+
+export const LogoImg = styled.img`
+  position: relative;
+  /* margin-top: 30px;
+  margin-left: 30px;
+  left: 100px; */
+  width: 200px;
+  object-fit: contain;
+  cursor: pointer;
+`
+
+export const LoginBtnContainer = styled.button`
+
+  position: relative;
+  /* left: 230px; */
+  width: 100px;
+  
+  font: Noto Sans KR;
+  display: block;
+  font-size: 14px;
+  text-align: center;
+  color: #fff;
+  padding: 8px 16px;
+
+  border-radius: 2px;
+  background: #e72f4b;
+`
+export const SignupBtnContainer = styled.button`
+  position: relative;
+  left: 10px;
+  width: 100px;
+
+  font: Noto Sans KR;
+  display: block;
+  font-size: 14px;
+  text-align: center;
+  color: #fff;
+  padding: 8px 16px;
+
+  border-radius: 2px;
+  background: #e72f4b;
+`
