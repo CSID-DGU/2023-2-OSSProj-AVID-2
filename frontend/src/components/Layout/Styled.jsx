649b9ffff40c8124e06d88dc488ec64a45514046
김범수 <imac@BMBA.local>
2023-11-02 09:34:22 +0900

    :lipstick: Design: 로그인 화면 수정
    상단 nav바 고정 후 로그인 화면 수정

--- frontend/src/components/Layout/Styled.jsx
+++ frontend/src/components/Layout/Styled.jsx
@@ -2,22 +2,23 @@ import styled, { keyframes } from "styled-components";
 
 // nav
 export const Container = styled.nav`
-  position: relative;
+  position: absolute;
   top: 0;
+
   width: 100%;
   height: 30px;
   padding-top: 20px;
   padding-bottom: 20px;
-  padding-right: 20px;
+  padding-left: 20px;
   display: flex;
-  align-items: center;
-  transition-timing-function: ease-in;
-  transition: all 0.5s;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  gap: 20px;
   background-color: white;
 `;
 
 export const LogoImg = styled.img`
-  position: relative;
+  //position: absolute;
   /* margin-top: 30px;
   margin-left: 30px;
   left: 100px; */
@@ -27,8 +28,6 @@ export const LogoImg = styled.img`
 `;
 
 export const LoginBtnContainer = styled.button`
-  position: relative;
-  /* left: 230px; */
   width: 100px;
 
   font: Noto Sans KR;
@@ -41,11 +40,10 @@ export const LoginBtnContainer = styled.button`
   border-radius: 2px;
   background: #e72f4b;
 `;
+
 export const SignupBtnContainer = styled.button`
-  position: relative;
-  left: 10px;
   width: 100px;
-
+  margin left: 1000px;
   font: Noto Sans KR;
   display: block;
   font-size: 14px;
@@ -56,3 +54,60 @@ export const SignupBtnContainer = styled.button`
   border-radius: 2px;
   background: #e72f4b;
 `;
+
+// nav
+// export const Container = styled.nav`
+//   position: relative;
+//   top: 0;
+//   width: 100%;
+//   height: 30px;
+//   padding-top: 20px;
+//   padding-bottom: 20px;
+//   padding-right: 20px;
+//   display: flex;
+//   align-items: center;
+//   transition-timing-function: ease-in;
+//   transition: all 0.5s;
+//   background-color: white;
+// `;
+
+// export const LogoImg = styled.img`
+//   position: relative;
+//   /* margin-top: 30px;
+//   margin-left: 30px;
+//   left: 100px; */
+//   width: 200px;
+//   object-fit: contain;
+//   cursor: pointer;
+// `;
+
+// export const LoginBtnContainer = styled.button`
+//   position: relative;
+//   /* left: 230px; */
+//   width: 100px;
+
+//   font: Noto Sans KR;
+//   display: block;
+//   font-size: 14px;
+//   text-align: center;
+//   color: #fff;
+//   padding: 8px 16px;
+
+//   border-radius: 2px;
+//   background: #e72f4b;
+// `;
+// export const SignupBtnContainer = styled.button`
+//   position: relative;
+//   left: 10px;
+//   width: 100px;
+
+//   font: Noto Sans KR;
+//   display: block;
+//   font-size: 14px;
+//   text-align: center;
+//   color: #fff;
+//   padding: 8px 16px;
+
+//   border-radius: 2px;
+//   background: #e72f4b;
+// `;
