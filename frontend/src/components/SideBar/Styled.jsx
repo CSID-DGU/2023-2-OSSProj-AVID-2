5cbb6ce2ab6f1fb00c0b781bc8d081ef13a1671e
김범수 <imac@BMBA.local>
2023-11-04 02:01:30 +0900

    :lipstick: Design: sidebar 화면 수정
    사이버 캠퍼스 사진 및 각 메뉴 추가

--- frontend/src/components/SideBar/Styled.jsx
+++ frontend/src/components/SideBar/Styled.jsx
@@ -1,31 +1,45 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const SideBarContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #f5f5f5;
-    padding: 20px;
-    width: 100px;
-    height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  padding: 20px;
+  width: 160px;
+  height: 100%;
 `;
 
 export const MenuList = styled.ul`
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
 `;
 
 export const MenuItem = styled.li`
-    padding: 10px;
-    font-size: 16px;
-    font-weight: bold;
-    color: #333;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
+  padding: 1px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: left;
+  color: #333;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 
-    &:hover {
-        background-color: #ddd;
-    }
+  &:hover {
+    background-color: #ddd;
+  }
+`;
+
+export const SidebarLogo = styled.img`
+  top: 0;
+  width: 200px;
+  height: 80px;
+`;
+
+export const ListLine = styled.div`
+  width: 170px;
+  height: 3px;
+  border-radius: 5px;
+  background-color: #ad9191;
 `;
