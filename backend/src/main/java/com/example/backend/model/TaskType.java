b9766ed375d0cfeb5936d329f02b43158bbf4e00
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-11-04 16:21:21 +0900

    :bug: Feat: complete 오류 수정 및 메서드, 변수 이름 변경

--- backend/src/main/java/com/example/backend/model/TaskType.java
+++ backend/src/main/java/com/example/backend/model/TaskType.java
@@ -1,12 +1,11 @@
 package com.example.backend.model;
 
 public enum TaskType {
-    ASSIGNMENT, PRESENTATION, TEST, STUDY;
+    ASSIGNMENT, PRESENTATION, TEST;
 
     public static TaskType returnType(String type){
         if(type.equals("ASSIGNMENT")) return TaskType.ASSIGNMENT;
         if(type.equals("PRESENTATION")) return TaskType.PRESENTATION;
-        if(type.equals("STUDY")) return TaskType.STUDY;
         return TEST;
     }
 }
