e151833264c5306a8eb3376e729a59b62e0a0332
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-11-04 22:32:33 +0900

    :bug: Fix: login, signup 요청 반환값 수정

--- backend/src/main/java/com/example/backend/config/AuthenticationConfig.java
+++ backend/src/main/java/com/example/backend/config/AuthenticationConfig.java
@@ -2,19 +2,11 @@ package com.example.backend.config;
 
 import com.example.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -26,18 +18,19 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers( "/login", "/singup", "/denied", "/resources/**").permitAll() // 로그인 권한은 누구나, resources파일도 모든권한
-                // USER, ADMIN 접근 허용
-                .antMatchers("/userAccess").hasRole("USER")
-                .antMatchers("/userAccess").hasRole("ADMIN")
+                    .authorizeRequests()
+                    .antMatchers("/login", "/signup", "/denied", "/resources/**").permitAll() // 로그인 권한은 누구나, resources 파일도 모든 권한
+                    // USER, ADMIN 접근 허용
+                    .antMatchers("/userAccess").hasRole("USER")
+                    .antMatchers("/userAccess").hasRole("ADMIN")
+                    .antMatchers("/personal").authenticated()
+
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/login_proc")
-                .defaultSuccessUrl("/access")
-                .failureUrl("/denied") // 인증에 실패했을 때 보여주는 화면 url, 로그인 form으로 파라미터값 error=true로 보낸다.
-                .and()
-                .csrf().disable(); //로그인 창
+                .csrf().disable(); // 로그인 창
     }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService); // UserService를 사용하여 사용자 정보를 로드하고 인증
+    }
+
 }
