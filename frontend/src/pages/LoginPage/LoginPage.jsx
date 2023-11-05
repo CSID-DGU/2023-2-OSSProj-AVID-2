67a771f7a7eab2991bb5bbb9bdd7d70a12811ca5
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-11-05 20:28:47 +0900

    :sparkles: Feat: 회원가입/로그인 페이지 navigate 함수 추가

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import * as s from "./Styled.jsx";
+import API from "../../api/axios.jsx";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [ID, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
@@ -21,8 +23,21 @@ const LoginPage = () => {
             userID: ID,
             userPwd: password
         });
+        
+        console.log("login Success");
         console.log(response);
-        navigate("/")
+
+        const sessionId = response.data.sessionId; // 서버에서 세션 ID를 받음
+        localStorage.setItem("sessionId", sessionId); // 세션 ID를 저장
+        console.log("session");
+        try{
+          console.log(sessionId);
+        }catch(error){
+          console.log("session Not Found");
+          console.error(error);
+        }
+        console.log(response);
+        navigate("/");
 
     } catch (error) {
         console.log("error");
