399758adb814dd1de221c9d207d69b0b7a8eea1c
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-11-12 16:08:19 +0900

    :art: Feat: TaskType 변경

--- backend/src/main/java/com/example/backend/model/TaskType.java
+++ backend/src/main/java/com/example/backend/model/TaskType.java
@@ -1,11 +1,11 @@
 package com.example.backend.model;
 
 public enum TaskType {
-    ASSIGNMENT, PRESENTATION, TEST;
+    INDIVIDUAL, TEAM; // 개인과제랑 팀과제를 나누는 용도
 
     public static TaskType returnType(String type){
-        if(type.equals("ASSIGNMENT")) return TaskType.ASSIGNMENT;
-        if(type.equals("PRESENTATION")) return TaskType.PRESENTATION;
-        return TEST;
+        if(type.equals("INDIVIDUAL")) return TaskType.INDIVIDUAL;
+        if(type.equals("TEAM")) return TaskType.TEAM;
+        return null;
     }
 }
