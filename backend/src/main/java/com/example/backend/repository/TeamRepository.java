821c2e3bf770b7318f137617b0dfc53fca7cdb3f
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-11-29 10:03:54 +0900

    :sparkles: Feat: 팀 생성 및 팀원 추가 기능

--- backend/src/main/java/com/example/backend/repository/TeamRepository.java
+++ backend/src/main/java/com/example/backend/repository/TeamRepository.java
@@ -1,6 +1,8 @@
 package com.example.backend.repository;
 
-// 유저 받아오기
-// 팀원 받아오기
-public interface TeamRepository {
+import com.example.backend.entity.TeamEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
+
 }
--- backend/src/main/java/com/example/backend/service/TeamService.java
+++ backend/src/main/java/com/example/backend/service/TeamService.java
@@ -1,19 +1,17 @@
 package com.example.backend.service;
 
 import com.example.backend.entity.*;
-import com.example.backend.exception.ErrorCode;
 import com.example.backend.repository.TeamRepository;
 import com.example.backend.repository.UserRepository;
 import com.example.backend.repository.UserSubjectRepository;
 import com.example.backend.repository.UserTeamRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.example.backend.exception.ErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -25,18 +23,13 @@ public class TeamService {
     @Autowired
     private UserSubjectRepository userSubjectRepository;
 
+    private final TeamRepository teamRepository;
+
+    private final UserRepository userRepository;
+
     // 각 과목을 수강하는 userName 반환
     public List<String> getUserNamesBySubjectId(Long subjectId) {
         return userSubjectRepository.findUserNamesBySubjectId(subjectId);
-    }
-
-    // 각 team_id를 가지는 userName 반환
-    public List<String> getUsersByTeam(TeamEntity team) {
-        List<UserTeamEntity> userTeamEntityList = userTeamRepository.findAllByTeam(team);
-
-        return userTeamEntityList.stream()
-                .map(userTeamEntity -> userTeamEntity.getUser().getUserName())
-                .collect(Collectors.toList());
     }
 
     // user_id에 해당하는 team의 subject_id로부터 subjectName을 가져오는 메소드
@@ -47,5 +40,30 @@ public class TeamService {
     // team_id에 해당하는 user_id로부터 userID, userName, userMajor를 가져오는 메소드
     public List<String> getUserInfoByTeamId(Long teamId) {
         return userTeamRepository.findUserInfoByTeamId(teamId);
+    }
+
+    // 팀 생성
+    public TeamEntity createTeam(SubjectEntity subject) {
+        TeamEntity team = TeamEntity.builder()
+                .subject(subject)
+                .build();
+
+        return teamRepository.save(team);
+    }
+
+    // 팀원 추가
+    public void addUserToTeam(Long userId, Long teamId) throws NotFoundException {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. ID: " + userId));
+
+        TeamEntity team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new NotFoundException("팀을 찾을 수 없습니다. ID: " + teamId));
+
+        UserTeamEntity userTeam = UserTeamEntity.builder()
+                .user(user)
+                .team(team)
+                .build();
+
+        userTeamRepository.save(userTeam);
     }
 }
