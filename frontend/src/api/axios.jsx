6ae2b649f295afadc1e345c1924e99ce4d4e6a46
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-12-02 18:57:17 +0900

    :sparkles: Feat: UserLogin 상태별  Nav바 수정"

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -8,20 +8,20 @@ export const API = axios.create({
   withCredentials: true
 });
 
-API.interceptors.request.use(
-  (config) => {
-    const sessionId = localStorage.getItem("sessionId");
-    if (sessionId) {
-      config.headers.Authorization = `Bearer ${sessionId}`;
-    }
-    return config;
-  },
-  (error) => {
-    console.error(error);
+// API.interceptors.request.use(
+//   (config) => {
+//     const sessionId = localStorage.getItem("sessionId");
+//     if (sessionId) {
+//       config.headers.Authorization = `Bearer ${sessionId}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     console.error(error);
     
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 
 export default API;
