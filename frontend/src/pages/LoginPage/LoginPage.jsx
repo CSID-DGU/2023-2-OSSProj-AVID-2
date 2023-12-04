c0a748024f04f124adf1bcf304dd64bcecce60f5
Kim Bum Soo <bumsookim333@naver.com>
2023-12-04 20:04:27 +0900

    :lipstick: Design: 로그인 화면 수정

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -18,56 +18,58 @@ const LoginPage = () => {
   async function handleSubmit(event) {
     event.preventDefault();
     try {
-        // POST 요청 코드
-        const response = await API.post('/login', {
-            userID: ID,
-            userPwd: password
-        });
-        
-        console.log("login Success");
-        console.log(response);
+      // POST 요청 코드
+      const response = await API.post("/login", {
+        userID: ID,
+        userPwd: password,
+      });
 
-        const sessionId = response.data.sessionId; // 서버에서 세션 ID를 받음
-        localStorage.setItem("sessionId", sessionId); // 세션 ID를 저장
-        console.log("session");
-        try{
-          console.log(sessionId);
-        }catch(error){
-          console.log("session Not Found");
-          console.error(error);
-        }
-        console.log(response);
-        navigate("/");
+      console.log("login Success");
+      console.log(response);
 
-    } catch (error) {
-        console.log("error");
+      const sessionId = response.data.sessionId; // 서버에서 세션 ID를 받음
+      localStorage.setItem("sessionId", sessionId); // 세션 ID를 저장
+      console.log("session");
+      try {
+        console.log(sessionId);
+      } catch (error) {
+        console.log("session Not Found");
         console.error(error);
+      }
+      console.log(response);
+      navigate("/");
+    } catch (error) {
+      console.log("error");
+      console.error(error);
     }
-};
+  }
 
   return (
     <s.Wrapper>
-      <s.Form onSubmit={handleSubmit}>
-        <s.Label>
-          <s.Input
-            type="text"
-            placeholder="아이디를 입력하세요"
-            value={ID}
-            onChange={handleUsernameChange}
-          />
-        </s.Label>
-        <br />
-        <s.Label>
-          <s.Input
-            type="password"
-            placeholder="비밀번호를 입력하세요"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </s.Label>
-        <br />
-        <s.SubmitButton type="submit">로그인</s.SubmitButton>
-      </s.Form>
+      <s.LoginFormContainer>
+        <s.Logo alt="DGU-Eclass-Logo" src="img/logo.png" />
+        <s.Form onSubmit={handleSubmit}>
+          <s.Label>
+            <s.Input
+              type="text"
+              placeholder="아이디를 입력하세요"
+              value={ID}
+              onChange={handleUsernameChange}
+            />
+          </s.Label>
+          <br />
+          <s.Label>
+            <s.Input
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </s.Label>
+          <br />
+          <s.SubmitButton type="submit">로그인</s.SubmitButton>
+        </s.Form>
+      </s.LoginFormContainer>
     </s.Wrapper>
   );
 };
