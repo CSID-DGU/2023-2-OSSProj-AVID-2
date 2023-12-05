a0bc31ed1fd45f1aa64f9c7bb28f9d6f655b2d75
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-12-05 14:42:03 +0900

    :art: Fix: session으로 유저 인증

--- backend/src/main/java/com/example/backend/controller/TaskController.java
+++ backend/src/main/java/com/example/backend/controller/TaskController.java
@@ -5,11 +5,13 @@ import com.example.backend.controller.request.TaskRequestDTO;
 import com.example.backend.controller.response.EclassResponseDTO;
 import com.example.backend.controller.response.Response;
 import com.example.backend.controller.response.TaskDetailResponseDTO;
+import com.example.backend.controller.response.UserLoginResponseDTO;
 import com.example.backend.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 // 과제 일정 api
@@ -20,21 +22,22 @@ public class TaskController {
     private final ScheduleService scheduleService;
 
     @PostMapping("")
-    public Response<Void> createTask(@RequestBody TaskRequestDTO requestDTO){
-
-        String userID = "2019111602";
-        scheduleService.writeTask(requestDTO, userID);
+    public Response<Void> createTask(@RequestBody TaskRequestDTO requestDTO, HttpSession session){
+        UserLoginResponseDTO loginUser = (UserLoginResponseDTO) session.getAttribute("loginUser");
+        scheduleService.writeTask(requestDTO, loginUser.getUserID());
 
         return Response.success();
     }
 
     @GetMapping("")
-    public Response<List<EclassResponseDTO>> getSchedule(@RequestParam("subjectName") String subjectName, Authentication authentication) {
-        return Response.success(scheduleService.getTask(authentication.getName(), subjectName));
+    public Response<List<EclassResponseDTO>> getSchedule(@RequestParam("subjectName") String subjectName, HttpSession session) {
+        UserLoginResponseDTO loginUser = (UserLoginResponseDTO) session.getAttribute("loginUser");
+        return Response.success(scheduleService.getTask(loginUser.getUserID(), subjectName));
     }
 
     @GetMapping("/{scheduleId}")
-    public Response<TaskDetailResponseDTO> getScheduleDetail(@PathVariable Long scheduleId, Authentication authentication){
-        return Response.success(scheduleService.getTaskDetail(authentication.getName(), scheduleId));
+    public Response<TaskDetailResponseDTO> getScheduleDetail(@PathVariable Long scheduleId, HttpSession session){
+        UserLoginResponseDTO loginUser = (UserLoginResponseDTO) session.getAttribute("loginUser");
+        return Response.success(scheduleService.getTaskDetail(loginUser.getUserID(), scheduleId));
     }
 }
