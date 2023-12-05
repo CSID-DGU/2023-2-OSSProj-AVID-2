525af6830784c884a99264a4fa301747514d36f1
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-12-05 19:28:30 +0900

    :fire: Fix: sessionId 삭제

--- backend/src/main/java/com/example/backend/controller/response/UserLoginResponseDTO.java
+++ backend/src/main/java/com/example/backend/controller/response/UserLoginResponseDTO.java
@@ -12,12 +12,4 @@ public class UserLoginResponseDTO {
     private String userID;
     private String userName;
     private String userType;
-    private String sessionId;
-
-    public UserLoginResponseDTO(String userID, String userName, String userType) {
-        this.userID = userID;
-        this.userName = userName;
-        this.userType = userType;
-        this.sessionId = UUID.randomUUID().toString();
-    }
 }
