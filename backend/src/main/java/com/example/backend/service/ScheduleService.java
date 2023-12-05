69f9f358adf63b2190dbd1ba89ec1f229f54b79e
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2023-12-05 11:57:56 +0900

    :sparkles: Feat: 팀 스케줄 기능(생성, 삭제, 수정, 투두리스트) 추가

--- backend/src/main/java/com/example/backend/service/ScheduleService.java
+++ backend/src/main/java/com/example/backend/service/ScheduleService.java
@@ -11,10 +11,12 @@ import com.example.backend.repository.*;
 
 import lombok.RequiredArgsConstructor;
 
+import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +28,14 @@ import java.util.stream.Collectors;
 public class ScheduleService {
     private final UserRepository userRepository;
     private final PersonalScheduleRepository personalScheduleRepository;
+    private final TeamScheduleRepository teamScheduleRepository;
     private final TaskRepository taskRepository;
     private final EntityManager em;
     private final SubjectRepository subjectRepository;
     private final UserSubjectRepository userSubjectRepository;
     private final UserTaskRepository userTaskRepository;
     private final ScheduleRepository scheduleRepository;
+    private final TeamRepository teamRepository;
 
     public ScheduleResponseDTO getAllSchedule(Month month, int year, String userID) {
         UserEntity user = userRepository.findByUserID(userID)
@@ -65,6 +69,7 @@ public class ScheduleService {
         return new ScheduleResponseDTO(pSchedules, tSchedules);
     }
 
+    // 개인 일정 생성, 수정, 삭제
     public void createPersonalSchedule(CreateScheduleRequestDTO requestDTO, String userID) {
         UserEntity user = userRepository.findByUserID(userID)
                 .orElseThrow(() -> new Exception(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", userID)));
@@ -93,16 +98,14 @@ public class ScheduleService {
                 .orElseThrow(() -> new Exception(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", userID)));
         ScheduleEntity schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new Exception(ErrorCode.SCHEDULE_NOT_FOUND));
-        String scheduleType = scheduleRepository.findScheduleType(scheduleId);
-        if (scheduleType.equals("COMMON"))
-            return ScheduleDetailResponseDTO.fromPersonalSchedule((PersonalScheduleEntity) schedule);
-        return ScheduleDetailResponseDTO.fromTask((TaskEntity) schedule);
+        return ScheduleDetailResponseDTO.fromPersonalSchedule((PersonalScheduleEntity) schedule);
     }
 
+    // 과목 일정(임시)
     public void writeTask(TaskRequestDTO requestDTO, String userId) {
         UserEntity user = userRepository
                 .findByUserID(userId)
-                .orElseThrow(() -> new Exception(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", userId)));
+                .orElseThrow(() -> new Exception(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유저가 없습니다.", userId)));
         SubjectEntity subject = subjectRepository
                 .findBySubjectName(requestDTO.getSubjectName())
                 .orElseThrow(() -> new Exception(ErrorCode.SUBJECT_NOT_FOUND, String.format("%s 과목이 존재하지 않습니다.", requestDTO.getSubjectName())));
@@ -139,5 +142,44 @@ public class ScheduleService {
                 .orElseThrow(() -> new Exception(ErrorCode.SCHEDULE_NOT_FOUND));
         return TaskDetailResponseDTO.taskResponseDTO(schedule);
     }
+
+    // 팀 일정 생성, 삭제, 수정
+    public void createTeamSchedule(CreateScheduleRequestDTO requestDTO, Long teamId) throws NotFoundException {
+        TeamEntity team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new NotFoundException("팀을 찾을 수 없습니다. ID: " + teamId));
+        TeamScheduleEntity schedule = TeamScheduleEntity.fromTeamScheduleDTO(requestDTO, team);
+        teamScheduleRepository.save(schedule);
+    }
+
+    public void deleteTeamSchedule(Long scheduleId, Long teamId) throws NotFoundException {
+        TeamEntity team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new NotFoundException("팀을 찾을 수 없습니다. ID: " + teamId));
+        TeamScheduleEntity schedule = teamScheduleRepository.findByIdAndTeam(scheduleId, team)
+                .orElseThrow(() -> new Exception(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%d 팀이 작성한 %d 일정을 찾을 수 없습니다.", teamId, scheduleId)));
+        teamScheduleRepository.delete(schedule);
+    }
+
+    public void modifyTeamSchedule(Long scheduleId, Long teamId, ModifyScheduleRequestDTO requestDTO) throws NotFoundException {
+        TeamEntity team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new NotFoundException("팀을 찾을 수 없습니다. ID: " + teamId));
+        TeamScheduleEntity teamSchedule = teamScheduleRepository.findByIdAndTeam(scheduleId, team)
+                .orElseThrow(() -> new Exception(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%d 팀이 작성한 %d 일정을 찾을 수 없습니다.", teamId, scheduleId)));
+        teamSchedule.modifySchedule(requestDTO);
+    }
+
+    public ScheduleDetailResponseDTO getTeamDetail(Long teamId, Long scheduleId) throws NotFoundException {
+        TeamEntity team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new NotFoundException("팀을 찾을 수 없습니다. ID: " + teamId));
+        ScheduleEntity schedule = scheduleRepository.findById(scheduleId)
+                .orElseThrow(() -> new Exception(ErrorCode.SCHEDULE_NOT_FOUND));
+        return ScheduleDetailResponseDTO.fromTeamSchedule((TeamScheduleEntity) schedule);
+    }
+
+    // 투두리스트
+    public List<TeamScheduleEntity> getToDoList() {
+        LocalDate currentDate = LocalDate.now();
+        return teamScheduleRepository.findByDate(currentDate);
+    }
+
 }
 
