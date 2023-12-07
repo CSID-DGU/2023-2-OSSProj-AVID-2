706060b510e857c7f134b5e6497618f1a814e095
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-12-07 23:28:52 +0900

    :sparkles: Feat: 메인 화면 컴포넌트 추가, 수강 강좌 리스트 띄우기

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,17 +1,93 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import * as s from "./Styled.jsx";
+import API from "../../api/axios.jsx";
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vw;
-  background: url("img/main_bg.jpg") no-repeat;
-  background-size: cover;
-  background-position: center center;
-  background-attachment: fixed;
-  position: relative;
-  overflow: hidden;
-`;
 
 export default function MainPage() {
-  return <Container></Container>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  const [lectureList, setLectureList] = useState([]);
+  
+  const getLectureList = async () => {
+    try {
+      const response = await API.get("/subjects");
+      if(response.data.resultCode === "SUCCESS"){
+        console.log(response.data.result)
+        setLectureList(response.data.result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await API.get("/home");
+
+        if (response.data.resultCode === "SUCCESS") {
+          setIsLoggedIn(true);
+          setUserInfo(response.data.result);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (error) {
+        console.error("Error checking login status:", error);
+      }
+    };
+
+    checkLoginStatus();
+    getLectureList();
+  }, []);
+
+  return (
+    <s.Container>
+      <s.NoticeContinaer>
+        <s.TitleBox>공지사항</s.TitleBox>
+        
+          <s.NoticeList>eclass 인증서 로그인 매뉴얼</s.NoticeList>
+          <s.NoticeList>e-class 재시험 매뉴얼</s.NoticeList>
+          <s.NoticeList>e-Class 과제 및 시험 등록 매뉴얼</s.NoticeList>
+          <s.NoticeList>[안내] 이클래스 사용을 위한 [Internet Explorer] 및 [Chrome] 설정 방법</s.NoticeList>
+          <s.NoticeList>[안내] 학습자 업로드 자료실 관련</s.NoticeList>
+
+      </s.NoticeContinaer>
+
+      {isLoggedIn ? (
+        <>
+          {/* Content for logged-in user */}
+          <s.LectureContainer>
+            <s.TitleBox>내 강의실</s.TitleBox>
+            <s.LectureList>
+              {lectureList.map((lecture, index) => (
+                <p key={index}>{lecture}</p>
+              ))}
+            </s.LectureList>
+          </s.LectureContainer>
+
+          <s.DueLectureContainer>
+            <s.TitleBox>의무 교육 강의실</s.TitleBox>
+            {/* Render content for due lecture */}
+          </s.DueLectureContainer>
+        </>
+      ) : (
+        <>
+          {/* Content for non-logged-in user */}
+          <s.LectureContainer>
+            <s.TitleBox>내 강의실</s.TitleBox>
+            <s.ImageBox> </s.ImageBox>
+            <s.NoticeText>로그인 후 사용 가능합니다.</s.NoticeText>
+            {/* You can add an image or any content for non-logged-in state */}
+          </s.LectureContainer>
+
+          <s.DueLectureContainer>
+            <s.TitleBox>의무 교육 강의실</s.TitleBox>
+            <s.ImageBox> </s.ImageBox>
+            <s.NoticeText>로그인 후 사용 가능합니다.</s.NoticeText>
+            {/* You can add an image or any content for non-logged-in state */}
+          </s.DueLectureContainer>
+        </>
+      )}
+    </s.Container>
+  );
 }
