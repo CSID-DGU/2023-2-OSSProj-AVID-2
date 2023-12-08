fc5748aeb794d8cf3d29a1903e81dd52a390f7a4
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-12-08 23:02:46 +0900

    :art: Rename: AddTeam.jsx /Teampage로 이동

--- frontend/src/components/Calendar/TeamCalendar.jsx
+++ frontend/src/components/Calendar/TeamCalendar.jsx
@@ -4,7 +4,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import * as s from "./Styled.jsx";
-import AddTeam from "./AddTeam.jsx";
+import AddTeam from "../TeamPage/AddTeam.jsx";
 
 function TeamCalendar() {
   const events = [
